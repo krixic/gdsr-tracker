28dc417af5faf174705b1bfaa121370f50449645
Kayden <krixolodeon@gmail.com>
2024-06-08 20:51:14 +1000

    Line 5:10:  'DLCPage' is defined but never used  no-unused-vars YOU SUCK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! LET ME BUILD

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Header } from "./components/header/header.jsx";
 import { Footer } from "./components/footer/footer.jsx";
 import { HomePage } from "./pages/HomePage.jsx";
-import { DLCPage } from "./pages/DLCPage.jsx";
+// import { DLCPage } from "./pages/DLCPage.jsx";
 import { ProgressPage } from "./pages/ProgressPage.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
