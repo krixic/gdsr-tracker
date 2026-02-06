af4f9b4caaa4e75c2fb6a9e078efcf87a856f20c
krixic <krixolodeon@gmail.com>
2026-02-06 22:10:20 +1100

    data reference hotfix

---
+++ src/data/index.js
@@ -0,0 +1,11 @@
+import { gdsrLevels } from "./gdsr.js";
+import { dlcLevels } from "./dlc.js";
+import { ccplLevels } from "./ccpl.js";
+
+const datasets = {
+    gdsr: gdsrLevels,
+    dlc: dlcLevels,
+    ccpl: ccplLevels,
+};
+
+export default datasets;
