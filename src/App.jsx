fbde496e3b2f8e9fac57b27a2b3ab2f7e606798f
krixic <krixolodeon@gmail.com>
2026-02-06 22:07:43 +1100

    fix import component issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Header } from "./components/header.jsx";
-import { Footer } from "./components/footer.jsx";
+import { Header } from "./components/Header.jsx";
+import { Footer } from "./components/Footer.jsx";
 import { Stats } from "./pages/Stats.jsx";
 import { Info } from "./pages/Info.jsx";
 import { Settings } from "./pages/Settings.jsx";
